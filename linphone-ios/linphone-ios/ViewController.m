80c89550a70afd2adce349f044c364f325f209ce
DoubleMagicXu <2528167446@qq.com>
2018-03-08 14:53:36 +0800

    linphone-ios

--- linphone-ios/linphone-ios/ViewController.m
+++ linphone-ios/linphone-ios/ViewController.m
@@ -12,7 +12,6 @@
 @property (weak, nonatomic) IBOutlet UITextField *account;
 @property (weak, nonatomic) IBOutlet UITextField *password;
 - (IBAction)registration:(id)sender;
-
 @end
 
 @implementation ViewController
@@ -30,9 +29,32 @@
 //委托协议方法
 -(BOOL)textFieldShouldReturn:(UITextField *)textField{
     [textField resignFirstResponder];
-    NSLog(@"返回");
+    NSLog(@"点击return按键,返回");
     return YES;
 }
+
+//-(void)viewWillAppear:(BOOL)animated{
+//    [super viewWillAppear:animated];
+//    //注册键盘出现通知
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:)name:UIKeyboardDidShowNotification object:nil];
+//    //注册键盘隐藏通知
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyBoardDidHide:)name: UIKeyboardDidHideNotification object:nil];
+//}
+//-(void) viewWillDisappear:(BOOL)animated{
+//    [super viewWillDisappear:animated];
+//    //注销键盘出现通知
+//    [[NSNotificationCenter defaultCenter] removeObserver:self name: UIKeyboardDidShowNotification object:nil];
+//    //注销键盘隐藏通知
+//    [[NSNotificationCenter defaultCenter] removeObserver:self name: UIKeyboardDidHideNotification object:nil];
+//}
+//-(void) keyboardDidShoe:(NSNotification *)notification{
+//    NSLog(@"键盘已打开");
+//}
+//-(void) keyboardDidHide:(NSNotification *)notification{
+//    NSLog(@"键盘已关闭");
+//}
 - (IBAction)registration:(id)sender {
+    
 }
+
 @end
