cce50cb44e6738ff28c62882a6cbbe67025e9020
DoubleMagicXu <2528167446@qq.com>
2018-03-08 10:05:23 +0800

    linphone-ios

--- linphone-ios/linphone-ios/ViewController.m
+++ linphone-ios/linphone-ios/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+//委托协议
+@interface ViewController ()<UITextFieldDelegate,UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *account;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+- (IBAction)registration:(id)sender;
 
 @end
 
@@ -24,6 +27,12 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
+//委托协议方法
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    NSLog(@"返回");
+    return YES;
+}
+- (IBAction)registration:(id)sender {
+}
 @end
