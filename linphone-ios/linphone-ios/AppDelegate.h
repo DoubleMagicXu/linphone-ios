3cc9d1b6ca21358bbb6eae8fb5120a73ee206289
DoubleMagicXu <2528167446@qq.com>
2018-03-08 09:24:41 +0800

    linphone-ios

---
+++ linphone-ios/linphone-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  linphone-ios
+//
+//  Created by sqxu on 08/03/2018.
+//  Copyright © 2018 DoublerMagicXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ linphone-ios/linphone-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  linphone-ios
+//
+//  Created by sqxu on 08/03/2018.
+//  Copyright © 2018 DoublerMagicXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
