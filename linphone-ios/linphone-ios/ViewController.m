449bf5d9e650ca266fb3e868bd2b12076c705766
DoubleMagicXu <2528167446@qq.com>
2018-03-14 17:03:12 +0800

    linphone-ios

--- linphone-ios/linphone-ios/ViewController.m
+++ linphone-ios/linphone-ios/ViewController.m
@@ -22,11 +22,38 @@ static void registration_state_changed(struct _LinphoneCore *lc, LinphoneProxyCo
            ,linphone_proxy_config_get_identity(cfg)
            ,linphone_proxy_config_get_addr(cfg));
 }
+//call
+static void call_state_changed(LinphoneCore *lc, LinphoneCall *call, LinphoneCallState cstate, const char *msg){
+    switch(cstate){
+        case LinphoneCallOutgoingRinging:
+            printf("It is now ringing remotely !\n");
+            break;
+        case LinphoneCallOutgoingEarlyMedia:
+            printf("Receiving some early media\n");
+            break;
+        case LinphoneCallConnected:
+            printf("We are connected !\n");
+            break;
+        case LinphoneCallStreamsRunning:
+            printf("Media streams established !\n");
+            break;
+        case LinphoneCallEnd:
+            printf("Call is terminated.\n");
+            break;
+        case LinphoneCallError:
+            printf("Call failure !");
+            break;
+        default:
+            printf("Unhandled notification %i\n",cstate);
+    }
+}
 @interface ViewController ()<UITextFieldDelegate,UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *account;
 @property (weak, nonatomic) IBOutlet UITextField *password;
 - (IBAction)registration:(id)sender;
 - (IBAction)cancel:(id)sender;
+- (IBAction)call:(id)sender;
+- (IBAction)receive:(id)sender;
 
 @end
 
@@ -189,5 +216,52 @@ static void registration_state_changed(struct _LinphoneCore *lc, LinphoneProxyCo
     running=FALSE;
     NSLog(@"点击了注销按钮");
     
+}
+
+- (IBAction)call:(id)sender {
+    dispatch_queue_t queue = dispatch_queue_create("call", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(queue, ^{
+        LinphoneCall *call=NULL;
+        LinphoneCoreVTable vtable={0};
+        LinphoneCore *lc;
+        const char *dest="1001@119.29.238.47";
+        signal(SIGINT,stop);
+#ifdef DEBUG
+        linphone_core_enable_logs(NULL); /*enable liblinphone logs.*/
+#endif
+        lc=linphone_core_new(&vtable,NULL,NULL,NULL);
+        if (dest){
+            /*
+             Place an outgoing call
+             */
+            call=linphone_core_invite(lc,dest);
+            if (call==NULL){
+                printf("Could not place call to %s\n",dest);
+                goto end;
+            }else printf("Call to %s is in progress...",dest);
+            linphone_call_ref(call);
+        }
+        /* main loop for receiving notifications and doing background linphonecore work: */
+        while(running){
+            linphone_core_iterate(lc);
+            ms_usleep(50000);
+        }
+        if (call && linphone_call_get_state(call)!=LinphoneCallEnd){
+            /* terminate the call */
+            printf("Terminating the call...\n");
+            linphone_core_terminate_call(lc,call);
+            /*at this stage we don't need the call object */
+            linphone_call_unref(call);
+        }
+    end:
+        printf("Shutting down...\n");
+        linphone_core_destroy(lc);
+        printf("Exited\n");
+        
+    });
+   
+}
+
+- (IBAction)receive:(id)sender {
 }
 @end
